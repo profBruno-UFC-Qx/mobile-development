6cf4b2a24ee374f3942c54569443a469345ee07d
Bruno Mateus <brunomateus@gmail.com>
2026-04-01 19:20:37 -0300

    Mostrando projetos aprovados

--- assets/js/projetosAprovados.js
+++ assets/js/projetosAprovados.js
@@ -2,21 +2,19 @@ document.addEventListener('readystatechange', event => {
 
     // When window loaded ( external resources are loaded too- `css`,`src`, etc...) 
     if (event.target.readyState === "complete") {
-        const appUrl = "https://script.google.com/macros/s/AKfycbwa2JkI5j0jAH6apaztT8PO2wLuUjelxcsE9WiVAjSOCDCrReQCW3Yyo-7DgWD3_iCfdg/exec";
-        const sheetUrl = "1kbtETCCR_BzUKkZvAi_Jh-Ewy9GXWSMEPR0TzrVf9Wc"
+        const appUrl = "https://script.google.com/macros/s/AKfycbwQvGEuLzv8uDbY4lJ9rR4ZUWc7OGNpd3slFrb1TF2vbbDlEjzxadLnyvwL7BgkxHVXIQ/exec"
+        const sheetUrl = "1NmIb-XELIpxZlbk6zWT68LXai8M8RZdndYWRy811WGk"
 
         if(sheetUrl != ""){
             const url = appUrl + "?id=" + sheetUrl ;
             const table = document.getElementById("aprovados");
-
             fetch(url)
             .then(data => {return data.clone().json();})
             .then(res =>  { 
                 const descriptions = res['descriptions'];
-                const projects = res['projects'];
                 for(let i in descriptions) {
 
-                    if(projects[i][0].length == 0 ) continue;
+                    if(descriptions[i][0].length == 0 ) continue;
 
                     const row = table.insertRow(-1);
                     const number = row.insertCell(0);
@@ -24,8 +22,8 @@ document.addEventListener('readystatechange', event => {
                     const desc = row.insertCell(2);
 
                     number.appendChild(document.createTextNode(parseInt(i) + 1));
-                    project.appendChild(document.createTextNode(projects[i]));
-                    desc.appendChild(document.createTextNode(descriptions[i]));
+                    project.appendChild(document.createTextNode(descriptions[i][0]));
+                    desc.appendChild(document.createTextNode(descriptions[i][1]));
 
                 }
             })
